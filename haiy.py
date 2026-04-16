875687193f63636633be3736388615e36a03e760
孫海洋 <s744129991@outlook.com>
2026-04-17 00:14:05 +0800

    在合法范围内完全随机取 16 个起点”，每个起点之间没有固定间隔，可能相邻、可能重复

--- haiy.py
+++ haiy.py
@@ -1,3 +1,4 @@
+import torch
 #读数据
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
@@ -30,3 +31,34 @@ def decode(index):
 #测试
 print(encode('秦牧'))
 print(decode(encode('秦牧')))
+#转张量
+data = torch.tensor(encode(text), dtype=torch.long)
+print(data.shape)
+#切分数据
+n = int(0.9 * len(data))
+train_data = data[:n]
+val_data = data[n:]
+
+print(train_data.shape)
+print(val_data.shape)
+
+
+batch_size = 16  # 并发数
+block_size = 32  # 上下文窗口
+
+def get_batch(split):
+    if split == 'train':
+        data = train_data
+    else:
+        #在合法范围内完全随机取 16 个起点”，每个起点之间没有固定间隔，可能相邻、可能重复一共取 16 段，然后堆叠成二维张量。
+        data = val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i + block_size] for i in ix])
+    y = torch.stack([data[i + 1:i + block_size + 1] for i in ix])
+    return x, y
+    
+xb, yb = get_batch('train')
+print("输入 x 的形状:", xb.shape) 
+print("目标 y 的形状:", yb.shape)
+
+
